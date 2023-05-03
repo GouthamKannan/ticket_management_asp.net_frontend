351507ae088cf5e6dfd3c82519c40f89cb7d4327
Goutham Kannan <54542723+GouthamKannan@users.noreply.github.com>
2023-05-03 17:40:49 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const configs = {
-    api_url : "http://ticketmanagement.somee.com"
+    // api_url : "http://ticketmanagement.somee.com"
+    api_url : "https://www.ticketmanagement.somee.com"
 }
 
 export default configs
